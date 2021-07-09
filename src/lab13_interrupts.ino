3c3a3c3b710b1c29c6c8ac140cce835775230107
AKK2112 <aleckk18@gmail.com>
2021-07-09 15:04:06 -0500

    first commit

---
+++ src/lab13_interrupts.ino
@@ -0,0 +1,66 @@
+#include "oled-wing-adafruit.h"
+OledWingAdafruit display;
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(MANUAL);
+volatile bool interruptOccured = false;
+
+volatile int count = 0;
+
+void isr()
+{
+  count++;
+  interruptOccured = true;
+}
+
+bool press = false;
+
+void setup()
+{
+  pinMode(D6, INPUT_PULLUP); //counter increaser
+  pinMode(D5, INPUT);        //interrupter
+  pinMode(D7, OUTPUT);
+  attachInterrupt(D6, isr, FALLING);
+  Serial.begin(9600);
+  display.setup();
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Test");
+  display.display();
+}
+
+void loop()
+{
+  display.loop();
+  
+  if (interruptOccured)
+  {
+    interruptOccured = false;
+    Serial.println(count);
+    display.clearDisplay();
+    display.setTextSize(3);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println(count);
+    display.display();
+    delay(500);
+  }
+  if (digitalRead(D5) == LOW)
+    {
+      if (press == false) 
+      {
+        noInterrupts();
+        press = true;
+      } 
+        else 
+      {
+        interrupts();
+        press = false;
+      }
+      Serial.println("pressed");
+      //digitalWrite(D7, HIGH);
+      delay(500);
+  }
+  delay(1000);  
+}
